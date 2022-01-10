b0e072a00b18bf9bd15a382fad239d6ca5256bbf
brianpaperless <brian@thepaperlessagent.com>
2022-01-10 09:10:34 -0600

    first commmit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,165 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import Purchased from "./Purchased";
 
 function App() {
+  const [items, setItems] = useState([]);
+  const [purchases, setPurchases] = useState([]);
+  const [input, setInput] = useState("");
+  const [datalist, setDatalist] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/items/")
+      .then((response) => response.json())
+      .then((data) => {
+        setItems(data.items);
+        setPurchases(data.purchases);
+        setDatalist(data.datalist);
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    fetch("http://localhost:8000/api/items/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        name: input,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setItems((prevState) => [...prevState, data]);
+        setInput("");
+        if (!datalist.map((item) => item.name).includes(input)) {
+          setDatalist((prevState) => [...prevState, { name: input }]);
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleDelete(itemToDelete) {
+    fetch(`http://localhost:8000/api/item/${itemToDelete.id}/`, {
+      method: "DELETE",
+    })
+      .then((res) => {
+        if (res.status === 204 && !itemToDelete.is_purchased) {
+          setItems((prevItems) =>
+            prevItems.filter((item) => item.id !== itemToDelete.id)
+          );
+        } else if (res.status === 204 && itemToDelete.is_purchased) {
+          setPurchases((prevPurchases) =>
+            prevPurchases.filter((purchase) => purchase.id !== itemToDelete.id)
+          );
+        }
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handlePurchase(itemId) {
+    fetch(`http://localhost:8000/api/item/${itemId}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        is_purchased: true,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setItems((prevItems) => prevItems.filter((item) => item.id !== itemId));
+        setPurchases((prevPurchases) => [...prevPurchases, data]);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  function handleRestore(itemId) {
+    fetch(`http://localhost:8000/api/item/${itemId}/`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        is_purchased: false,
+      }),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setPurchases((prevPurchases) =>
+          prevPurchases.filter((item) => item.id !== itemId)
+        );
+        setItems((prevItems) => [...prevItems, data]);
+      })
+      .catch((err) => console.log(err));
+  }
+
+  const dates = Array.from(
+    new Set(
+      purchases.map((purchase) =>
+        new Date(purchase.purchased_at).toLocaleDateString()
+      )
+    )
+  );
+
+  const itemChoices = datalist.map((item) => item.name.toLowerCase()).sort();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main>
+      <h1>Shopping List</h1>
+      <form onSubmit={handleSubmit}>
+        <input
+          list="items"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <datalist id="items">
+          {itemChoices.map((item, index) => (
+            <option key={index} value={item} />
+          ))}
+        </datalist>
+        <button disabled={!input}>
+          <i className="fas fa-plus"></i>
+        </button>
+      </form>
+
+      <section>
+        <h2>To Purchase</h2>
+        {!items.length && <p>All done!</p>}
+        {items.map((item, index) => (
+          <div key={item.id} className="row">
+            <span className="item">{item.name}</span>
+            <div className="buttonGroup">
+              <button onClick={() => handlePurchase(item.id)}>
+                {" "}
+                <i className="fas fa-check"></i>
+              </button>
+              <button
+                className="deleteButton"
+                onClick={() => handleDelete(item)}
+              >
+                <i className="fas fa-trash"></i>
+              </button>
+            </div>
+          </div>
+        ))}
+      </section>
+
+      {purchases.length > 0 && <h2>Recent Purchases</h2>}
+      {dates.map((date) => (
+        <Purchased
+          key={date}
+          date={date}
+          handleRestore={handleRestore}
+          handleDelete={handleDelete}
+          items={purchases.filter(
+            (item) => new Date(item.purchased_at).toLocaleDateString() === date
+          )}
+        />
+      ))}
+    </main>
   );
 }
 
---
+++ src/Purchased.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function Purchased({
+  items,
+  date,
+  handleRestore,
+  handleDelete,
+}) {
+  return (
+    <section className="purchased">
+      <h2>{date}</h2>
+      {items.map((item, index) => (
+        <div key={item.id} className="row">
+          <span className="item">{item.name}</span>
+          <div className="buttonGroup">
+            <button
+              className="restoreButton"
+              onClick={() => handleRestore(item.id)}
+            >
+              <i className="fas fa-undo"></i>
+            </button>
+            <button className="deleteButton" onClick={() => handleDelete(item)}>
+              <i className="fas fa-trash"></i>
+            </button>
+          </div>
+        </div>
+      ))}
+    </section>
+  );
+}
